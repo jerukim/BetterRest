4b8f6cd5832d3967f973de67de9f4ed824d0ad4c
jerukim <mikurej95@gmail.com>
2020-11-02 15:58:12 -0500

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Jeru Kim on 11/2/20.
+//  Copyright © 2020 Jeru Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
